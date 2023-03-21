0c0a5d0268a8c117ea2149f594f0e3a8b8ed7d75
Dave Greber <davegreber@gmail.com>
2023-03-21 16:29:24 -0400

    youtube

--- scripts.js
+++ scripts.js
@@ -1,87 +1,60 @@
 const container = document.getElementById('container');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 container.appendChild(renderer.domElement);
 
 camera.position.z = 5;
 
-const videoURLs = [
-    'https://youtu.be/FI0hTjtM8mo',
-    'https://youtu.be/3VM_kh6y-Dc',
-    'https://youtu.be/ElVgL9dbUwQ',
-    'https://youtu.be/yez2nnQ7Buc',
-    'https://youtu.be/URWSWHZoS60',
+const videoIds = [
+    'FI0hTjtM8mo',
+    '3VM_kh6y-Dc',
+    'ElVgL9dbUwQ',
+    'yez2nnQ7Buc',
+    'URWSWHZoS60',
 ];
 
-const videoTextures = videoURLs.map((url) => {
-    const video = document.createElement('video');
-    video.src = url;
-    video.loop = true;
-    video.muted = true;
-    video.load();
+clet players = [];
 
-    return new THREE.VideoTexture(video);
-});
+function onYouTubeIframeAPIReady() {
+  videoIds.forEach((videoId, index) => {
+    const player = new YT.Player(videoId, {
+      width: '640',
+      height: '360',
+      videoId: videoId,
+      events: {
+        'onReady': () => {
+          createThumbnail(player, index);
+        }
+      }
+    });
 
-const thumbnailMeshes = videoTextures.map((texture, index) => {
-    const geometry = new THREE.PlaneGeometry(1, 1);
+    players.push(player);
+  });
+}
+
+function createThumbnail(player, index) {
+  const thumbnailUrl = `https://img.youtube.com/vi/${videoIds[index]}/hqdefault.jpg`;
+
+  const loader = new THREE.TextureLoader();
+  loader.setCrossOrigin('');
+  loader.load(thumbnailUrl, (texture) => {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
     const material = new THREE.MeshBasicMaterial({ map: texture });
     const mesh = new THREE.Mesh(geometry, material);
 
-    mesh.position.set(Math.sin((index / videoURLs.length) * Math.PI * 2) * 3, 0, Math.cos((index / videoURLs.length) * Math.PI * 2) * 3);
+    mesh.position.set(Math.sin((index / videoIds.length) * Math.PI * 2) * 3, 0, Math.cos((index / videoIds.length) * Math.PI * 2) * 3);
     mesh.lookAt(scene.position);
 
     scene.add(mesh);
-    return mesh;
-});
 
-function onMouseClick(event) {
-    event.preventDefault();
-
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-    raycaster.setFromCamera(mouse, camera);
-
-    const intersects = raycaster.intersectObjects(thumbnailMeshes);
-
-    if (intersects.length > 0) {
-        const selectedThumbnail = intersects[0].object;
-
-        if (selectedThumbnail.scale.x === 1) {
-            selectedThumbnail.scale.set(4, 4, 4);
-            selectedThumbnail.material.map.play();
-        } else {
-            selectedThumbnail.scale.set(1, 1, 1);
-            selectedThumbnail.material.map.pause();
-        }
-    }
+    animate();
+  });
 }
-
-window.addEventListener('click', onMouseClick);
 
 function animate() {
-    requestAnimationFrame(animate);
-
-    const elapsedTime = clock.getElapsedTime();
-
-    thumbnailMeshes.forEach((mesh, index) => {
-        mesh.rotation.y = elapsedTime * 0.2 + index * Math.PI / videoURLs.length;
-    });
-
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
-
-animate();
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-window.addEventListener('resize', onWindowResize);
